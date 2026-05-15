8ec48db8d14139095d3a09d7e4fdb7585913a952
戈德斯文 <gedesiwen@qq.com>
2026-05-15 16:36:31 +0800

    refactor: rewrite routes with ChatLayout and ConsoleLayout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,48 +1,37 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import AppShell from "./layouts/AppShell";
+import ChatLayout from "./layouts/ChatLayout";
+import ConsoleLayout from "./layouts/ConsoleLayout";
 import AgentsPage from "./pages/AgentsPage";
-import ArchitecturePage from "./pages/ArchitecturePage";
 import ChannelsPage from "./pages/ChannelsPage";
 import ChatPage from "./pages/ChatPage";
+import ConsoleDashboard from "./pages/ConsoleDashboard";
 import EventsPage from "./pages/EventsPage";
 import MemoryPage from "./pages/MemoryPage";
-import ProfilesPage from "./pages/ProfilesPage";
 import SettingsPage from "./pages/SettingsPage";
 import SkillsPage from "./pages/SkillsPage";
 import TasksPage from "./pages/TasksPage";
 import ToolsPage from "./pages/ToolsPage";
-import {
-  AGENTS_PATH,
-  ARCHITECTURE_PATH,
-  CHANNELS_PATH,
-  EVENTS_PATH,
-  MEMORY_PATH,
-  PROFILES_PATH,
-  SETTINGS_PATH,
-  SKILLS_PATH,
-  TASKS_PATH,
-  TOOLS_PATH,
-} from "./lib/sessionRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AppShell />}>
+        <Route element={<ChatLayout />}>
           <Route index element={<ChatPage />} />
           <Route path="sessions/:sessionId" element={<ChatPage />} />
-          <Route path={ARCHITECTURE_PATH.slice(1)} element={<ArchitecturePage />} />
-          <Route path={MEMORY_PATH.slice(1)} element={<MemoryPage />} />
-          <Route path={PROFILES_PATH.slice(1)} element={<ProfilesPage />} />
-          <Route path={AGENTS_PATH.slice(1)} element={<AgentsPage />} />
-          <Route path={CHANNELS_PATH.slice(1)} element={<ChannelsPage />} />
-          <Route path={TOOLS_PATH.slice(1)} element={<ToolsPage />} />
-          <Route path={SKILLS_PATH.slice(1)} element={<SkillsPage />} />
-          <Route path={TASKS_PATH.slice(1)} element={<TasksPage />} />
-          <Route path={EVENTS_PATH.slice(1)} element={<EventsPage />} />
-          <Route path={SETTINGS_PATH.slice(1)} element={<SettingsPage />} />
-          <Route path="*" element={<ChatPage />} />
         </Route>
+        <Route path="console" element={<ConsoleLayout />}>
+          <Route index element={<ConsoleDashboard />} />
+          <Route path="agents" element={<AgentsPage />} />
+          <Route path="tasks" element={<TasksPage />} />
+          <Route path="tools" element={<ToolsPage />} />
+          <Route path="skills" element={<SkillsPage />} />
+          <Route path="memory" element={<MemoryPage />} />
+          <Route path="events" element={<EventsPage />} />
+          <Route path="channels" element={<ChannelsPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+        </Route>
+        <Route path="*" element={<ChatPage />} />
       </Routes>
     </BrowserRouter>
   );
